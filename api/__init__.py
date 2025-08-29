92d0d607e74205bba7d8edeaacd902444ca2c181
Tertius <94773757+compounder26@users.noreply.github.com>
2025-08-30 03:08:14 +0700

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,11 @@
+import os
 
-# Create __init__.py in utils directory
-with open("c:\\Users\\Tertius\\brain\\main\\alphaDataBank\\utils\\__init__.py", "w") as f:
+# Get the current working directory of the script
+current_dir = os.getcwd()
+
+# Construct the full path to the __init__.py file
+init_file_path = os.path.join(current_dir, "alphaDataBank", "utils", "__init__.py")
+
+# Create the __init__.py file
+with open(init_file_path, "w") as f:
     f.write("")
