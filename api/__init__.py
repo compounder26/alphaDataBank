1c147683d782b906dc654648390840f66701de30
Tertius <94773757+compounder26@users.noreply.github.com>
2025-09-02 00:33:52 +0700

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,7 @@ import os
 current_dir = os.getcwd()
 
 # Construct the full path to the __init__.py file
-init_file_path = os.path.join(current_dir, "alphaDataBank", "utils", "__init__.py")
+init_file_path = os.path.join(current_dir, "utils", "__init__.py")
 
 # Create the __init__.py file
 with open(init_file_path, "w") as f:
